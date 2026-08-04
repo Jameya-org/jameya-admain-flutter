397034e51d39347f6c25fe2a12cfe64731ffafcd
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-04 06:18:37 +0300

    feat: add CreateSavingsCircleView widget and update gradle.properties for Flutter migration

---
+++ lib/features/create_savings_circle/presentation/view/create_savings_circle_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateSavingsCircleView extends StatelessWidget {
+  const CreateSavingsCircleView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
